dcbb6bc9c252e5826391b2274fef86304d1ff3e7
My Name <gh.abd.com.207@gmail.com>
2023-10-15 08:41:52 +0300

    add a nivbar

--- website/client/src/App.js
+++ website/client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {navbar} from './components/navbar'
+import {Navbar} from './components/navbar'
 import React, { useState } from "react";
 
 function App() {
@@ -16,44 +16,34 @@ function App() {
   };
   
   return (
-   
+  <div>
+    <Navbar/>
     <div className="app-container">
       <div className="left-container">
         <div className="about-chatbot">
-           <form>
-            <h1>Login</h1>
+            <form>
+              <h1>Login</h1>
+              <h3 id = 'formleft'>Please Enter Details</h3>
           
-          {/* <i>
-            <br/><br></br>
-            Hit clear button to clear chats.
-            <br />
-            Click Here To{" "}
-            <a href="https://docs.google.com/document/d/1eoVq-6oOpIMmheI0IgRy-Ty8FYTkKH1j/edit?usp=sharing&ouid=102388778311955000420&rtpof=true&sd=true">
-              learn More
-            </a>
-          </i>
-          <button onClick={handleCloseChatbot}>Cloes Chatbot</button> */}
-            <h3 id = 'formleft'>Please Enter Details</h3>
+              <input 
+                type = 'text'
+                placeholder='Enter your email'
+                value = {userEmail}
+                onChange={(e) => setUserEmail(e.target.value)}
+                />
           
-            <input 
-              type = 'text'
-              placeholder='Enter your email'
-              value = {userEmail}
-              onChange={(e) => setUserEmail(e.target.value)}
-              />
-          
-            <input 
-              type = 'password'
-              placeholder='Enter your password'
-              value = {userPassowrd}
-              onChange={(e) => setUserPassowrd(e.target.value)}
-              />
-              <button type="submit">Login</button>
+              <input 
+                type = 'password'
+                placeholder='Enter your password'
+                value = {userPassowrd}
+                onChange={(e) => setUserPassowrd(e.target.value)}
+                />
+                <button type="submit">Login</button>
           </form>           
         </div>
       </div>
     </div>
- 
+  </div>
   );
 }
 
