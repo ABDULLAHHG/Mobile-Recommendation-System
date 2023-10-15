abbabe4573107c643e449a78fd958fdc81ccc115
Aboud <gh.abd.com.207@gmail.com>
2023-10-15 04:48:27 +0000

    create a sql file for database

---
+++ website/server/sql/data.sql
@@ -0,0 +1,17 @@
+CREATE TABLE data_engineers (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  full_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password_hash VARCHAR(255) NOT NULL,
+  date_of_birth DATE,
+  address VARCHAR(255),
+  phone_number VARCHAR(20)
+);
+
+INSERT INTO data_engineers (full_name, email, password_hash, date_of_birth, address, phone_number)
+VALUES ('ABOUD THE DUDE',
+        'ABDULLAH@aboud.com',
+        'Aboud',
+        '2003-08-20',
+        'Al-basra',
+        '+96407705856142');
