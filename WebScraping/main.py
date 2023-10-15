ff0be87def0dd0520c9e197688ecea7c4ca3848f
My Name <gh.abd.com.207@gmail.com>
2023-10-15 05:55:00 +0300

    add time sleep before the requst

--- WebScraping/main.py
+++ WebScraping/main.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import time 
 
+time.sleep(90)
 # https://m.gsmarena.com , https://www.kimovil.com/en/
 response = requests.get('https://www.kimovil.com/en/where-to-buy-xiaomi-redmi-note-13-pro-plus')
 soup = BeautifulSoup(response.content,'perser.html')
