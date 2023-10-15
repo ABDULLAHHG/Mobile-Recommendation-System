1224b671d9b258b6e28057ab91681d86c96945ad
My Name <gh.abd.com.207@gmail.com>
2023-10-15 05:57:01 +0300

    create a login website as frontend

--- website/client/src/App.js
+++ website/client/src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import {navbar} from './components/navbar'
+import React, { useState } from "react";
 
 function App() {
+  const [isChatbotVisible , setIsChabotVisible] = useState(false);
+  const [userEmail , setUserEmail] = useState("")
+  const [userPassowrd , setUserPassowrd] = useState("")
+  
+  const handleOpenChatbot = () => {
+    setIsChabotVisible(true);
+  };
+
+  const handleCloseChatbot = () => {
+    setIsChabotVisible(false);
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div className="app-container">
+      <div className="left-container">
+        <div className="about-chatbot">
+            <h1>Login</h1>
+          
+          {/* <i>
+            <br/><br></br>
+            Hit clear button to clear chats.
+            <br />
+            Click Here To{" "}
+            <a href="https://docs.google.com/document/d/1eoVq-6oOpIMmheI0IgRy-Ty8FYTkKH1j/edit?usp=sharing&ouid=102388778311955000420&rtpof=true&sd=true">
+              learn More
+            </a>
+          </i>
+          <button onClick={handleCloseChatbot}>Cloes Chatbot</button> */}
+            <h3 id = 'formleft'>Please Enter Details</h3>
+          
+            <input 
+              type = 'text'
+              placeholder='Enter your email'
+              value = {userEmail}
+              onChange={(e) => setUserEmail(e.target.value)}
+              />
+          
+            <input 
+              type = 'password'
+              placeholder='Enter your password'
+              value = {userPassowrd}
+              onChange={(e) => setUserPassowrd(e.target.value)}
+              />
+              <button type="submit">Login</button>
+           
+        </div>
+      </div>
     </div>
+ 
   );
 }
 
