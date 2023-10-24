25ee9ae533497a818b67e3472687770c88075d1e
Aboud <gh.abd.com.207@gmail.com>
2023-10-25 01:51:28 +0300

    Create a components UserInput and UserDateInput

---
+++ website/client/src/components/UserDateInput/UserDateInput.js
@@ -0,0 +1,11 @@
+import './UserDateInput.css'
+import React from 'react'
+
+export default function UserDateInput(props) {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
---
+++ website/client/src/components/UserInput/UserInput.js
@@ -0,0 +1,11 @@
+import './UserInput.css'
+import React from 'react'
+
+export default function UserInput(props) {
+  return (
+    <div>
+     
+    </div>
+  )
+}
+
