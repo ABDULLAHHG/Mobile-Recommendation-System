c005146d05f1dd5adb77f11251f7c2e244856f49
Aboud <gh.abd.com.207@gmail.com>
2023-10-24 18:23:23 +0300

    delete title and subtitle

--- website/client/src/components/Hero/Hero.js
+++ website/client/src/components/Hero/Hero.js
@@ -5,8 +5,8 @@ export default function Hero() {
   return (
     <div className='hero-main'>
         <div className='hero-text'>
-            <h6 className='hero-subtitle'> Hi </h6>
-            <h4 className='hero-title'><em>Hi</em></h4>
+            <h6 className='hero-subtitle'>  </h6>
+            <h4 className='hero-title'><em></em></h4>
 
         </div>
     </div>
