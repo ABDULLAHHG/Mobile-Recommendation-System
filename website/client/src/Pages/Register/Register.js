f42e3f9e653f9149b877f9be44ff7707a16dc71e
My Name <gh.abd.com.207@gmail.com>
2023-10-30 21:31:37 +0300

    edit on register page on front-end and add classes for each input

--- website/client/src/Pages/Register/Register.js
+++ website/client/src/Pages/Register/Register.js
@@ -1,68 +1,156 @@
-import React, { useState } from "react";
+import React, {useEffect , useState} from "react";
+import { useNavigate, NavLink } from "react-router-dom";
+import basestyle from "../Base.module.css";
 import './Register.css';
+import axios from "axios";
 
 function Register() {
-  const [userEmail , setUserEmail] = useState("")
-  const [userPassword , setUserPassword] = useState("")
-  const [userName , setUserName] = useState("")
-  const [userAdress , setUserAdress] = useState("")
+  const navigate = useNavigate();
 
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [user, setUserDetails] = useState({
+    fullName: "",
+    lastName: "",
+    address : "",
+    date  : "",
+    email: "",
+    password: "",
+    cpassword: "",
+  });
+
+  
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setUserDetails({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const signupHandler = (e) => {
+    e.preventDefault();
+    setFormErrors(validateForm(user));
+    setIsSubmit(true);
+    // if (!formErrors) {
+    //   setIsSubmit(true);
+    // }
+  };
+
+  useEffect(() => {
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(user);
+      axios.post("http://localhost:9002/signup/", user).then((res) => {
+        alert(res.data.message);
+        navigate("/login", { replace: true });
+      });
+    }
+  }, [formErrors]);
+
+
+  // validating user input  
+  const validateForm = (values) => {
+    const error = {};
+    const regex = /^[^\s+@]+@[^\s@]+\.[^\s@]{2,}$/i;
     
-  const register_sent = () => {
-    fetch('/Register' , {
-      method:['POST'],
-      headers:{
-        'Content-Type' :'application/json'
-      },
-      
-      body:JSON.stringify({
-      'userEmail' : userEmail,
-      'userAdress':userAdress,
-      'userName' : userName,
-      'userPassword' : userPassword
-    }),
+    if (!values.fullName) {
+      error.fullName = "First Name is required";
+    }
 
-    }).then({
-    })
-  }
+    if (!values.lastName) {
+      error.lastName = "Last Name is required";
+    }
+
+    if (!values.email) {
+      error.email = "Email is required";
+    } else if (!regex.test(values.email)) {
+      error.email = "This is not a valid email format!";
+    }
+
+    if (!values.password) {
+      error.password = "Password is required";
+    } else if (values.password.length < 4) {
+      error.password = "Password must be more than 4 characters";
+    } else if (values.password.length > 10) {
+      error.password = "Password cannot exceed more than 10 characters";
+    }
+
+    if (!values.cpassword) {
+      error.cpassword = "Confirm Password is required";
+    } else if (values.cpassword !== values.password) {
+      error.cpassword = "Confirm password and password should be same";
+    }
+    return error;
+  };
+
 
   return (
   <div>
     <div className="Register-app-container">
       <div className="Register-mid-container">
         <div className="Register">
-              <h1>Register</h1>
-              <h3 id = 'formleft'>Please Enter Details</h3>
-          
-              <input 
-                type = 'text'
-                placeholder='Enter your Name'
-                value = {userName}
-                onChange={(e) => setUserName(e.target.value)}
-                />
+          <form>
+            <h1>Create your account</h1>
+            <input
+            className="fullName"
+              type="text"
+              name="fname"
+              id="fname"
+              placeholder="First Name"
+              onChange={changeHandler}
+              value={user.fname}
+            />
             
-              <input 
-                type = 'text'
-                placeholder='Enter your Email'
-                value = {userEmail}
-                onChange={(e) => setUserEmail(e.target.value)}
-                />
-                
-              <input 
-                type = 'password'
-                placeholder='Enter your Adress'
-                value = {userAdress}
-                onChange={(e) => setUserAdress(e.target.value)}
-                />
+            <input
+              className="lastName"
+
+              type="text"
+              name="lname"
+              id="lname"
+              placeholder="Last Name"
+              onChange={changeHandler}
+              value={user.lname}
+            />
+            <p className={basestyle.error}>{formErrors.lname}</p>
+            <p className={basestyle.error}>{formErrors.fname}</p>
+            <input
+            className="email"
+            type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              onChange={changeHandler}
+              value={user.email}
+            />
+            <p className={basestyle.error}>{formErrors.email}</p>
             
-            <input 
-                type = 'password'
-                placeholder='Enter your password'
-                value = {userPassword}
-                onChange={(e) => setUserPassword(e.target.value)}
-                />
+            <input
+            className="password"
+            type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              onChange={changeHandler}
+              value={user.password}
+            />
+            <p className={basestyle.error}>{formErrors.password}</p>
             
-                <button type="submit" onClick={register_sent}>Login</button>
+            <input
+            className="cpassword"
+            type="password"
+              name="cpassword"
+              id="cpassword"
+              placeholder="Confirm Password"
+              onChange={changeHandler}
+              value={user.cpassword}
+            />
+            <p className={basestyle.error}>{formErrors.cpassword}</p>
+            
+            
+            <button className={basestyle.button_common} onClick={signupHandler}>
+              Register
+            </button>
+          </form>
         </div>
       </div>
     </div>
