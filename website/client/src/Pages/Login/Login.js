2d4c18556f6ffaf88497ef64266043e77565968d
Aboud <gh.abd.com.207@gmail.com>
2023-11-08 15:36:46 +0300

    edit on input and create user oop to save user input

--- website/client/src/Pages/Login/Login.js
+++ website/client/src/Pages/Login/Login.js
@@ -1,24 +1,42 @@
 import React, { useState } from "react";
 import './Login.css'
+import axios from 'axios'
 
 export function Login() {
-    const [userEmail , setUserEmail] = useState("")
-    const [userPassword , setUserPassword] = useState("")
+    // init the 
+    const [formErrors , setFormErrors] = useState({});
+    const [isSubmit , setIsSubmit] = useState(false);
+    const [user , setUserDetails] = useState({
       
+      userEmail : "",
+      userPassword : "",
+    });
+    
+    const changeHandler = (e) => {
+      const {name , value} = e.target;
+      setUserDetails({
+        ...user,
+        [name] : value
+      });
+    }
+
+
+    
     const register_sent = () => {
       fetch('/Register' , {
         method:['POST'],
         headers:{
           'Content-Type' :'application/json'
         },
-        body:JSON.stringify({'userEmail' : userEmail,
-                             'userPassword' : userPassword})
+        body:JSON.stringify({'userEmail' : user.userEmail,
+                             'userPassword' : user.userPassword})
       }).then({
         
       })
     }
   
     return (
+
     <div>
       <div className="Login-app-container">
         <div className="Login-mid-container">
@@ -27,17 +45,21 @@ export function Login() {
                 <h3 id = 'formleft'>Please Enter Details</h3>
             
                 <input 
+                  id = 'email'
+                  name = 'email'
                   type = 'text'
                   placeholder='Enter your email'
-                  value = {userEmail}
-                  onChange={(e) => setUserEmail(e.target.value)}
+                  value = {user.userEmail}
+                  onChange={changeHandler}
                   />
-            
+                            
                 <input 
+                  id = 'password'
+                  name = 'password'
                   type = 'password'
                   placeholder='Enter your password'
-                  value = {userPassword}
-                  onChange={(e) => setUserPassword(e.target.value)}
+                  value = {user.userPassword}
+                  onChange={changeHandler}
                   />
                   <button type="submit" onClick={register_sent}>Login</button>
                   <p>Havent Registered <a href="/Register">yet</a> ? </p>
