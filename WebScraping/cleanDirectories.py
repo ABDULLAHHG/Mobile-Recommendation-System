2ab9cce209e0de6667dbaef8815bd21662f4b9a3
ABOUD <gh.abd.com.207@gmail.com>
2023-11-17 22:09:00 +0300

    Create a function to move html files for each brand  for each directory

--- WebScraping/cleanDirectories.py
+++ WebScraping/cleanDirectories.py
@@ -14,3 +14,12 @@ def renameMainPage():
         # Rename a file
         os.rename(f"WebScraping/Data/GSMArena/HTML/{brand} phones.html", f"WebScraping/Data/GSMArena/HTML/{brand} phones Page1.html")
 
+
+def MoveBrandPageToMainDirectoryBrand():
+    df = pd.read_csv('WebScraping/Data/GSMArena/CSV/Pages for each Brand.csv')
+    brands = df.Brand
+    pages = df['Page Number'] 
+    for index , brand in enumerate(brands):
+        # Rename a file
+        os.rename(f"WebScraping/Data/GSMArena/HTML/{brand} phones Page{pages[index]}.html", f"WebScraping/Data/GSMArena/HTML/{brand}/{brand} phones Page{pages[index]}.html")
+MoveBrandPageToMainDirectoryBrand()
